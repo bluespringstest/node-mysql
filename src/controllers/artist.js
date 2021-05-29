5846cb9586c8d3d16d0582a779f6795cdcaf375c
bluespringstest <71788357+bluespringstest@users.noreply.github.com>
2021-05-29 12:04:05 +0100

    added variable to our statement as an array.

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -4,7 +4,10 @@ exports.create = async (req, res) => {
     const db = await getDb();
     const { name, genre } = req.body;
     try{
-        await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+        await db.query(`INSERT INTO Artist (name, genre) VALUES (?, ?)`, [
+            name,
+            genre,
+        ]);
         res.sendStatus(201);
     } catch (err) {
         res.sendStatus(500).json(err);
